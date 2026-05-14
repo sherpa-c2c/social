aae0de0d55ddb2ba919b82aea32d9e026a242274
Odoo Migration Factory <sherpa-c2c@users.noreply.github.com>
2026-05-14 23:38:36 +0200

    [MIG] mail_activity_board: Migration to 19.0

--- mail_activity_board/__manifest__.py
+++ mail_activity_board/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Mail Activity Board",
     "summary": "Add Activity Boards",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "development_status": "Beta",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
